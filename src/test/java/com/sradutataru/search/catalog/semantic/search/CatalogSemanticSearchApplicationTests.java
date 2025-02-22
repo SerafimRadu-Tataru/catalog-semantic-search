93c4a2527707c3e5a3b914384ea7edfd05cbee7e
Serafim Radu-Tataru <sradutataru@griddynamics.com>
2025-02-22 17:53:05 +0200

    Initial commit

---
+++ src/test/java/com/sradutataru/search/catalog/semantic/search/CatalogSemanticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sradutataru.search.catalog.semantic.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogSemanticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
